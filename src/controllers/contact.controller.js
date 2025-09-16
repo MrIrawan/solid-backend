82295027b88d1b3a3c725db6ee036c2927807565
farrel irawan <irawanssfarrel@gmail.com>
2025-09-16 12:09:54 +0700

    feat (contact): membuat controller createContactController

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -18,3 +18,22 @@ export const getAllContactsController = async (req, res, next) => {
         next(error);
     }
 }
+
+export const createContactController = async (req, res, next) => {
+    const contactData = req.body;
+
+    if (!contactData) return res.status(400).json({
+        status: "Failed To Create Contact",
+        message: "Contact data is required"
+    });
+
+    try {
+        const result = await createContact(contactData);
+        res.status(200).json({
+            status: "Successfully Created Contact",
+            data: result
+        })
+    } catch (error) {
+        next(error);
+    }
+}
