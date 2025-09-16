3f806ca629de33f34b9d878c8d9dbea01bb6018f
farrel irawan <irawanssfarrel@gmail.com>
2025-09-16 14:50:47 +0700

    fix (contact): menghapus beberapa parameter di controller dan service fitur contact

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,15 +1,8 @@
 import { getAllContacts, createContact } from "../services/contact.service.js";
 
 export const getAllContactsController = async (req, res, next) => {
-    const { email, password } = req.admin;
-
-    if (!email || !password) return res.status(401).json({
-        status: "Failed To Fetch All Contacts",
-        message: "Unauthorized user"
-    });
-
     try {
-        const result = await getAllContacts({ email, password });
+        const result = await getAllContacts();
         res.status(200).json({
             status: "Successfully Fetched All Contacts",
             data: result
