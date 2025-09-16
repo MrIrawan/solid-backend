83e60df8787078342f395b39bbc512f6648cc1a5
farrel irawan <irawanssfarrel@gmail.com>
2025-09-16 14:39:10 +0700

    menambahkan route endpoint masing masing fitur kedalam file server.js

--- src/server.js
+++ src/server.js
@@ -5,8 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8000;
 
+import { bookingRouter } from "./routes/booking.route.js";
+import { contactRouter } from "./routes/contact.route.js";
+
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/api", bookingRouter);
+app.use("/api", contactRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
