4a3d1904815a008f74e33dca2c5c69e4e930fde8
farrel irawan <irawanssfarrel@gmail.com>
2025-09-17 11:11:43 +0700

    fix (import module's): mengubah pemanggilan API supabase di file config supabase dan mengubah projek menjadi ESM

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,9 +1,9 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv"
 
 dotenv.config();
 
-export const supabase = new SupabaseClient(
+export const supabase = createClient(
     process.env.SUPABASE_PROJECT_URL,
     process.env.SUPABASE_ANON_KEY
 );
